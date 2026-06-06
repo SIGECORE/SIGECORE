4a7bd2c319ada5d18001f55ba516100025558444
karolhh <karoldayannahernandez@uts.edu.co>
2026-06-06 17:34:04 -0500

    merge: resolver conflictos

--- app/main.py
+++ app/main.py
@@ -1,15 +1,11 @@
-# app/main.py
 from fastapi import FastAPI
-from fastapi.security import HTTPBearer
-from api.v1.comunicados import router
+from api.v1.router import router
 
 app = FastAPI(
     title="SIGECORE API",
     description="API REST para gestión de conjuntos residenciales",
     version="1.0.0"
 )
-
-security = HTTPBearer()
 
 app.include_router(router, prefix="/api/v1")
 
